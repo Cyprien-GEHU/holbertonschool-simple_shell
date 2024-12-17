7577720c8fb2b823d1afffbac651429e238725e5
Cyprien <cypriengehu@example.com>
2024-12-17 08:57:06 +0000

    add file header shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_FILE
+#define SHELL_FILE
+
+#include <stdio.h>
+#include <sys/type.h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+#endif
