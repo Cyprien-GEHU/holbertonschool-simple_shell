24ef657ef03d627fa6143983dd55d05f5ae6b154
Cyprien <cypriengehu@example.com>
2025-01-06 10:24:34 +0000

    add the prompt file for the interface of simple shell

---
+++ prompt.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * input_read - we write the command
+ *
+ * Return: the command written
+ */
+
+char *input_read(void)
+{
+	char *command;
+	size_t n;
+	int line;
+
+	printf("$ ");
+	line = getline(&command, &n, stdin);
+	if (line == -1)
+	{
+		printf("error");
+		free(command);
+		return (NULL);
+	}
+
+	return (command);
+}
