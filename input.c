0f80e82654950390cdbce5e88b491cb3fdcfcacd
Cyprien <cypriengehu@example.com>
2025-01-07 10:27:45 +0000

    new file for read the input in simple shell

---
+++ input.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * input_read - we write the command
+ *
+ * Return: the command written
+ */
+
+char *input_read(void)
+{
+	char *command;
+	size_t n;
+	int line;
+
+	line = getline(&command, &n, stdin);
+	if (line == -1)
+	{
+		printf("error");
+		free(command);
+		return (NULL);
+	}
+
+	return (command);
+}
